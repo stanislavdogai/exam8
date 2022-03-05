8843da83ea50265d14ef0237389ed6a7a9a2ef7e
stanislavdogai <stanislavdogai01@gmail.com>
2022-03-05 18:47:48 +0600

    Обновил фикстуры, добавил немного данных

--- exam8/webapp/forms.py
+++ exam8/webapp/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from .models import Product, Review
 
@@ -11,6 +12,12 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         exclude = ['author', 'product', 'created_at', 'updated_at', 'check_moderated']
+
+    def clean_grade(self):
+        print(self.cleaned_data.get('grade'))
+        if self.cleaned_data.get('grade') < 1 or self.cleaned_data.get('grade') > 5:
+            raise ValidationError(f'Нужно ввести оценку в диапазоне от 1 до 5')
+        return self.cleaned_data.get('grade')
 
 class ReviewCheckForm(forms.ModelForm):
     class Meta:
